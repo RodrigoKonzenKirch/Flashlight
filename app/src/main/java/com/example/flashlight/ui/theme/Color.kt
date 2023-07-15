02ee1a768cfa5f4f77fd849a9fb0c81dc5e42bc6
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2023-07-15 15:13:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/flashlight/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.flashlight.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AppPrimaryDark = Color(0xFF49670B)
+val AppSecondaryDark = Color(0xFF596248)
+val AppTertiaryDark = Color(0xFF396661)
+val AppBackgroundDark = Color(0xFF1B1C17)
+val AppOnBackgroundDark = Color(0xFFE4E3DB)
+
+val AppPrimaryLight = Color(0xFFAFD36E)
+val AppSecondaryLight = Color(0xFFC1CAAB)
+val AppTertiaryLight = Color(0xFFA0D0C9)
+val AppBackgroundLight = Color(0xFFFEFCF4)
+val AppOnBackgroundLight = Color(0xFF1B1C17)
